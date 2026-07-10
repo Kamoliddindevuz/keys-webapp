32694c18e9dfb3c2c581028a3640469212dad342
Kamoliddindevuz <torayevkamoliddin10@gmail.com>
2026-07-10 10:40:09 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-keys-webapp/
-│
-├── index.html
-├── style.css
-├── script.js
-│
-├── api/
-│   ├── check.js
-│   ├── balance.js
-│   ├── case.js
-│
-├── images/
-│   ├── bronze.png
-│   ├── silver.png
-│   ├── gold.png
-│
-└── sounds/
-    └── open.mp3
+
