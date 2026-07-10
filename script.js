8b22f8f3af3a184064d5790c9b8b3f78454c448f
Kamoliddindevuz <torayevkamoliddin10@gmail.com>
2026-07-10 11:25:03 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,52 +1,88 @@
 let balance = Number(localStorage.getItem("balance")) || 1000;
 
-const rewards = [10,20,50,100,250,500];
-const chances = [30,25,20,15,8,2];
+document.getElementById("balance").innerHTML = balance + " 💎";
+
+const cases = {
+
+bronze:{
+price:100,
+rewards:[10,20,30,50,80,100,150,250,500]
+},
+
+silver:{
+price:500,
+rewards:[80,100,150,250,500,750,1000]
+},
+
+gold:{
+price:1000,
+rewards:[300,500,750,1000,1500,2000,5000]
+}
+
+};
+
+function updateBalance(){
+
+localStorage.setItem("balance",balance);
+
+document.getElementById("balance").innerHTML=
+balance+" 💎";
+
+}
+
+function randomReward(arr){
+
+return arr[Math.floor(Math.random()*arr.length)];
+
+}
+
+function openCase(type){
+
+const data=cases[type];
+
+if(balance<data.price){
+
+document.getElementById("result").innerHTML=
+"❌ Balans yetarli emas";
+
+return;
+
+}
+
+balance-=data.price;
 
 updateBalance();
 
-function updateBalance(){
-    document.getElementById("balance").innerText = balance;
-    localStorage.setItem("balance", balance);
-}
+const prize=randomReward(data.rewards);
 
-function randomReward(){
-    let rand = Math.random()*100;
-    let total = 0;
+const track=document.getElementById("track");
 
-    for(let i=0;i<rewards.length;i++){
-        total += chances[i];
-        if(rand <= total){
-            return rewards[i];
-        }
-    }
-    return 10;
-}
+track.style.transition="none";
 
-function openCase(){
-
-    if(balance < 100){
-        document.getElementById("result").innerHTML =
-        "❌ Balans yetarli emas";
-        return;
-    }
-
-    balance -= 100;
-
-    document.getElementById("result").innerHTML =
-    "🎰 Ochilmoqda...";
-
+track.style.transform="translateX(0px)";
     setTimeout(()=>{
 
-        const prize = randomReward();
+const items=document.querySelectorAll(".item");
 
-        balance += prize;
+const index=Math.floor(Math.random()*items.length);
 
-        updateBalance();
+const move=(index*110)-160;
 
-        document.getElementById("result").innerHTML =
-        "🎉 Siz yutdingiz <br><br>💎 "+prize;
+track.style.transition="transform 3s cubic-bezier(.17,.67,.19,1)";
 
-    },2500);
+track.style.transform=`translateX(-${move}px)`;
+
+},100);
+
+setTimeout(()=>{
+
+balance+=prize;
+
+updateBalance();
+
+document.getElementById("result").innerHTML=
+"🎉 Siz yutdingiz<br><br>💎 "+prize;
+
+},3200);
 
 }
