92dc86d0ab3ce630f5b86897f2d29977c1f1c419
Kamoliddindevuz <torayevkamoliddin10@gmail.com>
2026-07-10 10:46:06 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("Keys WebApp ishga tushdi!");
-});
+let balance = 1000;
+
+const prizes = [
+10,20,30,50,75,100,150,250
+];
+
+function openCase(){
+
+if(balance<100){
+document.getElementById("result").innerHTML="❌ Balans yetarli emas";
+return;
+}
+
+balance-=100;
+
+const prize=prizes[Math.floor(Math.random()*prizes.length)];
+
+balance+=prize;
+
+document.getElementById("balance").innerHTML=balance;
+
+document.getElementById("result").innerHTML=
+"🎉 Siz yutdingiz <br><br> 💎 "+prize;
+
+}
