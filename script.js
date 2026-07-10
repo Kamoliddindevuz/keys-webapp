4a0b8b9fd280753e42f0fb8626a2fa750a361b50
Kamoliddindevuz <torayevkamoliddin10@gmail.com>
2026-07-10 10:59:53 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,52 @@
-let balance = 1000;
+let balance = Number(localStorage.getItem("balance")) || 1000;
 
-const prizes = [10,20,50,100,250,500];
+const rewards = [10,20,50,100,250,500];
+const chances = [30,25,20,15,8,2];
+
+updateBalance();
+
+function updateBalance(){
+    document.getElementById("balance").innerText = balance;
+    localStorage.setItem("balance", balance);
+}
+
+function randomReward(){
+    let rand = Math.random()*100;
+    let total = 0;
+
+    for(let i=0;i<rewards.length;i++){
+        total += chances[i];
+        if(rand <= total){
+            return rewards[i];
+        }
+    }
+    return 10;
+}
 
 function openCase(){
 
     if(balance < 100){
-        document.getElementById("result").innerHTML="❌ Balans yetarli emas";
+        document.getElementById("result").innerHTML =
+        "❌ Balans yetarli emas";
         return;
     }
 
     balance -= 100;
 
-    const track = document.getElementById("track");
-
-    track.style.transition="none";
-    track.style.transform="translateX(0px)";
+    document.getElementById("result").innerHTML =
+    "🎰 Ochilmoqda...";
 
     setTimeout(()=>{
 
-        const prize = prizes[Math.floor(Math.random()*prizes.length)];
+        const prize = randomReward();
 
-        const index = prizes.indexOf(prize);
+        balance += prize;
 
-        const move = (index*100)-150;
+        updateBalance();
 
-        track.style.transition="transform 3s cubic-bezier(.17,.67,.19,1)";
-        track.style.transform=`translateX(-${move}px)`;
+        document.getElementById("result").innerHTML =
+        "🎉 Siz yutdingiz <br><br>💎 "+prize;
 
-        setTimeout(()=>{
-
-            balance += prize;
-
-            document.getElementById("balance").innerHTML=balance;
-
-            document.getElementById("result").innerHTML=
-            "🎉 Siz yutdingiz <br><br> 💎 "+prize;
-
-        },3000);
-
-    },100);
+    },2500);
 
 }
